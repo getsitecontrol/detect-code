c822619cc15d15fbc2d5b2f17b2670b637043701
arusanov <alexander.rusanov@gmail.com>
2017-11-06 14:29:32 +0300

    fixed import

--- src/entry.ts
+++ src/entry.ts
@@ -1,11 +1,11 @@
 import * as fastify from 'fastify'
-import url from 'url'
+import {parse as parseUrl} from 'url'
 import {detectCode} from './detect-code'
 
 const api = fastify()
 api.get('/', async function (request, reply) {
   const queryUrl = request.query.url
-  if (queryUrl && url.parse(queryUrl)) {
+  if (queryUrl && parseUrl(queryUrl)) {
     reply.send(await detectCode(request.query.url))
   } else {
     reply.code(500).send({error: 'bad url parameter'})
