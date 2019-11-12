200960f209cd86ea2a2be985fc5288330b3bf4b1
smant <smirnoff.anton@gmail.com>
2019-11-12 09:38:40 +0300

    ignore more unneeded resources

--- src/browser-scripts/common.ts
+++ src/browser-scripts/common.ts
@@ -6,7 +6,21 @@ const ignores = [
     /mc\.yandex\.ru\//,
     /vk\.com\/rtrg/,
 
-    /google-analytics\.com\/r\/collect/,
+    /google-analytics\.com\//,
+    /\.doubleclick\.net\/r\/collect\//,
+
+    /alexametrics\.com\//,
+    /hs-analytics\.net/,
+    /ct\.pinterest\.com/,
+    /\.mxpnl\.com\/libs/,
+    /api\.mixpanel\.com\/track/,
+    /.+\.hotjar\.com\//,
+
+    /track\.hubspot\.com/,
+    /analytics\.twitter\.com/,
+    /tr\.snapchat\.com/,
+    /pixel\.tapad\.com/,
+    /\.dlx\.addthis\.com\//,
 
 
     // ads
@@ -14,12 +28,21 @@ const ignores = [
     /google\.ru\/pagead\//,
     /\.doubleclick\.net\/pagead\//,
 
+    /\/t\.co\/i\//,
+
+    /ads\.linkedin\.com\//,
+    /\.ads-twitter\.com\//,
+
 
     // chat soft
     /[\w]\.livetex\.ru\//,
+    /[\w]\.olark\.com\//,
 
     // other
-    /cdn\.ravenjs\.com/
+    /cdn\.ravenjs\.com/,
+
+    /\/vk\.com\/images\//,
+    /\/vk\.com\/emoji\//,
 ]
 
 export function ignoreRequest(request: puppeteer.Request): boolean {
